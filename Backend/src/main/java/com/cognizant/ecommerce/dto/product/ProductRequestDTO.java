73538576811622efcd61750190b296e9551d4593
khss12345 <KallakuriHSS.SriHarshit@cognizant.com>
2025-09-24 16:32:59 +0530

    added controller

--- Backend/src/main/java/com/cognizant/ecommerce/dto/product/ProductRequestDTO.java
+++ Backend/src/main/java/com/cognizant/ecommerce/dto/product/ProductRequestDTO.java
@@ -18,8 +18,8 @@ public class ProductRequestDTO {
     private BigDecimal price;
     private Boolean isActive;
     private String image_url;
-    private Boolean isActive;
+
     private Long quantity;
-    private Boolean isActive;
+
     private Long categoryId;
 }
